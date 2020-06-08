4dbf949c0861d571d99f495f412803e9318c6a14
joe.dutra@donnet.host <bookingerbrasil@MacBook-Pro-de-Bookinger.local>
2020-06-08 02:59:05 -0300

    first version

---
+++ src/controllers/initializer.ts
@@ -0,0 +1,230 @@
+/*
+NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mMMMMMMMMMNNNmmNNNMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mmNMMNMMMMNNNNNmmmddhdddNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mddNMMNy:/odNmmddmmNNmdhhddmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmmdNMNd:--+dNmmddhhddmmhsyhhmdmmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmNmdNmy:.-oyNmmmhmdhho+sososyhhhddNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmmNdh+-`.:oyNNdmmdmmdo-://oysssyhhhdmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+Nmmmoyyyo+osdNmdmmddNNhs+/::/+osyssydyhdNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NNmhsymMMNmmmmdmdNNddNmsso+++////ossssyyhdmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mhhhmNNMNNNhssshhmmddmmssyooooso/::+oysshhhhmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mmdhdddNNdyoosyhdmddmmmsoooooyysyys/::/oyyhhhyMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mdddhddmhsooshdmdmdhhyyyysso/ooo+syhhs/-/+shyhMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+dyyhdmd+ososhdmdmyyhhhhhhhyo++o/+///+ohhso++sdMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+dhdmNNdsossyhmdmsydhssssyhhs/++o/o+//:++yhhy+/hNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mdmNNNNmhysshddyshdyyy/oss+s::/:://++///++++/::hmNNNNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NNMNNNmmNNdymNNhshdshdyhdysh+sy+-:++osssosss++yNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmNNNmdNNmNmmmNmyyddyyhdhydyohys/-oo+osssysyyohNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mmNNNhdNmmNNmNMMNhyyhhhdhyyhmmyh+-/s+sysssyyhyydNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mNMMMhdNdmMNMMMMMNNmdhdddmhdmmNho/-osoyyo++oyddhhNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NMMMNmhNdNMNMNMMNmNNNmmmdyoohmhoyo::hsooo++oooydhymMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMNNNhmNNMmmNMNNmmmmdmmdyhhoyddddoo++yoyysooossyhsmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMNNNmmNNNmdNdNmmddhhhdNNhsmNssdooo/dso++osyyysoymMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMNNNNmNNNNNmddmmNhshNmmmNmNMdhNsh/ohho++/:++MMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MNNNMMNNNNmmmhhhhdyosdNmdmMMhoNmhdmys+ooo++/+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mmNNNMMNNNNmddmdoodmMMNmmNNhssdmNMMMNdNd/osomMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmNdhMNmNNMNmdNddohmMMNNNmdmdddNMMMMMMMMmMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmNhmMmmmmNNmdNyoNMNmNmdhyyyhdhoyNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmdmMmmddddNmmdys+hmMMMmmhysssyy++dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmdNMMdmdddmmNNyshmNNNNNNNdhhs+yy//dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmNMMMdmdddmmMNysdmNNMMMNhhNdhs+y+/:mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mmNMMNhmmddNNNMdyydmMMMNdyshNhyoss+:/MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmNMMddmmmmNMNMNdsymNNmdhhdNMNdhsss+:yMMMMMMMMMMMMMMMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMdhmmmmmNMNNMmshNMMMmmMMMMMmNdyo+//NMMMMMMMMMMMMMMMhNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMmhmmmmmmNMMNNMyshdhhhyhNMMMMMMdhso+sMMMMMMMMMMMMMMMhmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMmdmmmmmmmNMMMmNm+ys++oyyNMMMMMMNmmyyoyNMMMMMMMMMMMMMddMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NmmmmmmmmmmmNMNNmNNyyo+/oohNMMMMMMMMdhhsshmMMMMMMMMMMMyNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+mmNNNNNNmmmmNMMNmmddNmmdhhdmMMMMMMMMMNddhssshmmNNNmmdhdMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NNNNNNNNNNNNNNNNmNNNNMMMMMNomMMMMMMMMMNNmdhhyyyyyyyhdmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+Nd+oNMMMMMMMmodo++++++++++m..yNMMMMMNo+mNMMmhssshdNMMNhNMMMMMMMMMMMddMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MN+ /NMMMMMm: d` -ssssss+`d. `+mMMMMN. dNm+:+syso//hNN--yNMMMMMMMd+`yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMN+ /NMMMm: oM` +NMMMMMNdN. /`.yNMMN. dh.omMMMMMNy.oM- `:hNMMMm+.  yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMN/ /NMm: oNy` :sssmMMMMN. dh-`/mMN. d-/NMMMMMMMMy`m- y/`/dmo..o: yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMN/ /m: +NNy. /yyyNMMMMN. dNNo`.yN- d.oNMMMMMMMMd d- mNh-`.`+mN/ yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMN/ . +NMMN- oNMMMMMNdN. dMMMd:`/. ds.dNMMMMMMm::M- dMMNy/dMMN/ yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMN/ +NMMMN- /yyyyyys d. dMMMMNo`  dNy-+ymmmho-+NN- dMMMMMMMMN/ yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMNyNMMMMN+::::::::::m+/mMMMMMMd: dMMNho///+ymMMN+/mMMMMMMMMNs/hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMMNsmMMMMMMMMMMMMMMNNNNMMNNNMMNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMMMMMMMMMMMMNMMNMNMMMNMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMNMNMMMNMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNMMNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+all copyright reservation for S2 Click, Inc
+*/
+import { readFileSync } from 'fs';
+import latestVersion from 'latest-version';
+import { Page } from 'puppeteer';
+import { from, interval, timer } from 'rxjs';
+import { map, takeUntil, tap, delay, switchMap } from 'rxjs/operators';
+import { Whatsapp } from '../api/whatsapp';
+import { CreateConfig, defaultOptions } from '../config/create-config';
+import { upToDate } from '../utils/semver';
+import { isAuthenticated, isInsideChat, needsToScan, retrieveQR } from './auth';
+import { initWhatsapp, injectApi } from './browser';
+import chalk = require('chalk');
+import boxen = require('boxen');
+import Spinnies = require('spinnies');
+const { version } = require('../../package.json');
+
+// Global
+let updatesChecked = false;
+
+/**
+ * consult status of whatsapp client
+ */
+
+/**
+ * Should be called to initialize whatsapp client
+ */
+export async function create(
+  session = 'session',
+  catchQR?: (qrCode: string, asciiQR: string) => void,
+  statusFind?: (statusGet: string) => void,
+  options?: CreateConfig
+) {
+  const spinnies = new Spinnies();
+
+  console.log(`\n
+▗▄ ▄ ▄  ▄▄▄ ▄   ▄▄▄  ▗▄▄▖▗▄▖ ▗▄▖▄▄▄▖                           
+ █▟█▙█  █▄▄ █   █    █ ▐█ █ █ █ █▄▄                           
+ ▐█ █▌  █▄▄ ███ ▜▄▛▘ ▜▙▟▀ █   █ █▄▄▖                           
+                                                      
+▄▄▄▖       ▄▄▄▄▄▄▄▄▄▄▄▟▌▀▖     ▗▄▄▖                        
+▝▄ ▐▖     ▟▘▐         ▐▌ ▝▙    ▐  ▌ ▄▞▀▀▀▀▀▄▖ ▛▀▄        ▄▘▌
+ ▝▄ ▐▖   ▐▘ ▟▖ ▜▀▀▀▀▀▌▐▖  ▝▚▖  ▐  ▙▞▘▗▞▀▀▀▚▖▝▙▛ ▝▜▖    ▗▀  ▌
+  ▀▖ ▜  ▗▘ ▟█  ▜     ▝▐▌ ▟▖ ▜▖ ▐  █ ▟▘     ▐▖▝█   ▝▙▖▗▞▘   ▌
+   ▜▖ ▜▄▛ ▐▜▝  ▝ ▘▜   ▝▌ ▟▀▖ ▝▙▐  ▌▗▌       ▜ ▙ ▐▙▖ ▀▘ ▗▟▘ ▌
+    ▚▖ ▀ ▗▌▝▜▖ ▜▀▀▀   ▐▌ ▟ ▝▙  ▜  ▙ ▙       ▛ ▙ ▐▌▝▄  ▟▀▐▘ ▌
+     ▚▖ ▗▛  ▐▖ ▜    ▗▛▜▌ ▟   ▚▖   █▖▝▙▖   ▗▛ ▟▛ ▐▖  ▀▞  ▐▖ ▌
+     ▝▚▗▛   ▐   ▘▘▘▘▘ ▐▌ ▟    ▀▄  ▌▝▙▖▝▀▀▀▘▗▞▘▛ ▐▌      ▐  ▌
+      ▝▛    ▝▀▝▘▘▘▘▘▘▀▝▘▘▀     ▝▙ ▌  ▝▀▘▘▀▀▘  ▀▘▀       ▝▀▝▘
+                                 ▚▌
+                                   \n`);
+
+  // Check for updates if needed
+  if (!updatesChecked) {
+    spinnies.add('venom-version-spinner', {
+      text: '🕷🕷🕷Checking for updates🕷🕷🕷',
+    });
+    checkSullaVersion(spinnies);
+    updatesChecked = true;
+  }
+
+  // Initialize whatsapp
+  spinnies.add(`${session}-auth`, {
+    text: '🕷🕷🕷Waiting...🕷🕷🕷',
+  });
+
+  const mergedOptions = { ...defaultOptions, ...options };
+  let waPage = await initWhatsapp(session, mergedOptions);
+
+  spinnies.update(`${session}-auth`, { text: '🕷🕷🕷Authenticating...🕷🕷🕷' });
+  const authenticated = await isAuthenticated(waPage);
+
+  // If not authenticated, show QR and wait for scan
+  if (authenticated) {
+    // Wait til inside chat
+    if (statusFind) {
+      statusFind('isLogged');
+    }
+
+    await isInsideChat(waPage).toPromise();
+    spinnies.succeed(`${session}-auth`, { text: '🕷🕷🕷Authenticated🕷🕷🕷' });
+  } else {
+    if (statusFind) {
+      statusFind('notLogged');
+    }
+    spinnies.update(`${session}-auth`, {
+      text: `Authenticate to continue`,
+    });
+
+    if (mergedOptions.refreshQR <= 0) {
+      const { data, asciiQR } = await retrieveQR(waPage);
+      if (catchQR) {
+        catchQR(data, asciiQR);
+      }
+
+      if (mergedOptions.logQR) {
+        console.log(`Scan QR for: ${session}                `);
+        console.log(asciiQR);
+      }
+    } else {
+      grabQRUntilInside(waPage, mergedOptions, session, catchQR);
+    }
+
+    // Wait til inside chat
+    await isInsideChat(waPage).toPromise();
+    spinnies.succeed(`${session}-auth`, { text: '🕷🕷🕷Compilation Mutation🕷🕷🕷' });
+  }
+  spinnies.add(`${session}-inject`, { text: '🕷🕷🕷Injecting Sibionte...🕷🕷🕷' });
+  waPage = await injectApi(waPage);
+  spinnies.succeed(`${session}-inject`, { text: 'Starting With Success!' });
+
+  if (mergedOptions.debug) {
+    const debugURL = `http://localhost:${readFileSync(
+      `./${session}/DevToolsActivePort`
+    ).slice(0, -54)}`;
+    console.log(`\nDebug: \x1b[34m${debugURL}\x1b[0m`);
+  }
+
+  return new Whatsapp(waPage);
+}
+
+function grabQRUntilInside(
+  waPage: Page,
+  options: CreateConfig,
+  session: string,
+  catchQR: (qrCode: string, asciiQR: string) => void
+) {
+  const isInside = isInsideChat(waPage);
+  timer(0, options.refreshQR)
+    .pipe(
+      takeUntil(isInside),
+      switchMap(() => retrieveQR(waPage))
+    )
+    .subscribe(({ data, asciiQR }) => {
+      if (catchQR) {
+        catchQR(data, asciiQR);
+      }
+      if (options.logQR) {
+        console.clear();
+        console.log(`Scan QR for: ${session}                `);
+        console.log(asciiQR);
+      }
+    });
+}
+
+/**
+ * Checs for a new versoin of venom and logs
+ */
+function checkSullaVersion(spinnies) {
+  latestVersion('venom-bot').then((latest) => {
+    if (!upToDate(version, latest)) {
+      logUpdateAvailable(version, latest);
+    }
+
+    spinnies.succeed('venom-version-spinner', { text: 'Checking for updates' });
+  });
+}
+
+/**
+ * Logs a boxen of instructions to update
+ * @param current
+ * @param latest
+ */
+function logUpdateAvailable(current: string, latest: string) {
+  // prettier-ignore
+  const newVersionLog = 
+  `There is a new version of ${chalk.bold(`Venom`)} ${chalk.gray(current)} ➜  ${chalk.bold.green(latest)}\n` + 
+  `Update your package by running:\n\n` +
+  `${chalk.bold('\>')} ${chalk.blueBright('npm update @s2click/venom')}`;
+
+  console.log(boxen(newVersionLog, { padding: 1 }));
+  console.log(
+    `For more info visit: ${chalk.underline(
+      'https://github.com/s2click/venom/blob/master/UPDATES.md'
+    )}\n`
+  );
+}
