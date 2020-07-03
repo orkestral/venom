e9cab0ba87ad17a8266fbfce9ae42bf1be6e2305
hbdbim <hbdbim@gmail.com>
2020-07-03 16:19:34 -0400

    downloadFile adjustment
    downloadFile get file if message isMMS

--- src/api/whatsapp.ts
+++ src/api/whatsapp.ts
@@ -59,73 +59,73 @@ import { ControlsLayer } from './layers/controls.layer';
 import { Message } from './model';
 
 declare module WAPI {
-  const arrayBufferToBase64: (buffer: ArrayBuffer) => string;
+    const arrayBufferToBase64: (buffer: ArrayBuffer) => string;
 }
 
 export class Whatsapp extends ControlsLayer {
-  constructor(public page: Page) {
-    super(page);
-  }
-
-  /**
-   * Get the puppeteer page instance
-   * @returns The Whatsapp page
-   */
-  get waPage(): Page {
-    return this.page;
-  }
-
-  /**
-   * Clicks on 'use here' button (When it get unlaunched)
-   * This method tracks the class of the button
-   * Whatsapp web might change this class name over the time
-   * Dont rely on this method
-   */
-  public async useHere() {
-    await this.page.waitForFunction(
-      () => {
-        const useHereClass = '.S7_rT.FV2Qy';
-        return document.querySelector(useHereClass);
-      },
-      { timeout: 0 }
-    );
-
-    await this.page.evaluate(() => {
-      const useHereClass = '.S7_rT.FV2Qy';
-      (<any>document.querySelector(useHereClass)).click();
-    });
-  }
-
-  /**
-   * Closes page and browser
-   */
-  public async close() {
-    if (this.page) {
-      await this.page.close();
+    constructor(public page: Page) {
+        super(page);
     }
 
-    if (this.page.browser) {
-      await this.page.browser().close();
+    /**
+     * Get the puppeteer page instance
+     * @returns The Whatsapp page
+     */
+    get waPage(): Page {
+        return this.page;
     }
-  }
 
-  /**
-   * Decrypts message file
-   * @param message Message object
-   * @returns Decrypted file buffer (null otherwise)
-   */
-  public async downloadFile(message: Message) {
-    if (message.isMedia) {
-      const url = message.clientUrl;
-      const encBase64 = await this.page.evaluate((url: string) => {
-        return fetch(url)
-          .then((response) => response.arrayBuffer())
-          .then((bytes) => WAPI.arrayBufferToBase64(bytes));
-      }, url);
+    /**
+     * Clicks on 'use here' button (When it get unlaunched)
+     * This method tracks the class of the button
+     * Whatsapp web might change this class name over the time
+     * Dont rely on this method
+     */
+    public async useHere() {
+        await this.page.waitForFunction(
+            () => {
+                const useHereClass = '.S7_rT.FV2Qy';
+                return document.querySelector(useHereClass);
+            },
+            { timeout: 0 }
+        );
 
-      return decrypt(encBase64, message);
-    } else {
-      return null;
+        await this.page.evaluate(() => {
+            const useHereClass = '.S7_rT.FV2Qy';
+            (<any>document.querySelector(useHereClass)).click();
+        });
     }
-  }
+
+    /**
+     * Closes page and browser
+     */
+    public async close() {
+        if (this.page) {
+            await this.page.close();
+        }
+
+        if (this.page.browser) {
+            await this.page.browser().close();
+        }
+    }
+
+    /**
+     * Decrypts message file
+     * @param message Message object
+     * @returns Decrypted file buffer (null otherwise)
+     */
+    public async downloadFile(message: Message) {
+        if (message.isMedia || message.isMMS) {
+            const url = message.clientUrl;
+            const encBase64 = await this.page.evaluate((url: string) => {
+                return fetch(url)
+                    .then((response) => response.arrayBuffer())
+                    .then((bytes) => WAPI.arrayBufferToBase64(bytes));
+            }, url);
+
+            return decrypt(encBase64, message);
+        } else {
+            return null;
+        }
+    }
 }
