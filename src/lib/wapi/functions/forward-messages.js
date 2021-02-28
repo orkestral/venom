4b3511bf4dfba7b15d38424cbc6de58307662259
Jónalan de Lima <jonalan7@gmail.com>
2021-02-28 03:28:38 -0300

    fix: forwardMessages return

--- src/lib/wapi/functions/forward-messages.js
+++ src/lib/wapi/functions/forward-messages.js
@@ -60,6 +60,7 @@ export async function forwardMessages(chatId, messages, skipMyMessages) {
   if (!Array.isArray(messages)) {
     messages = [messages];
   }
+
   var toForward = (
     await Promise.all(
       messages.map(async (msg) => {
@@ -68,37 +69,58 @@ export async function forwardMessages(chatId, messages, skipMyMessages) {
     )
   ).filter((msg) => (skipMyMessages ? !msg.__x_isSentByMe : true));
 
-  if (chat.id) {
-    await Promise.each(toForward, async (e) => {
-      let conversation = await window.Store.Chat.get(chatId);
-      let newId = await window.WAPI.getNewMessageId(chat.id);
-      let tempMsg = await Object.create(
-        chat.msgs.filter((msg) => msg.__x_isSentByMe)
-      )[0];
-      let toFor = await Object.assign(e);
-      let extend = {
-        id: newId,
-        from: Store.Me.wid,
-        t: parseInt(new Date().getTime() / 1000),
-        to: chat.__x_contact.__x_id,
-        ack: 0,
-        participant: void 0,
-        local: !0,
-        self: 'out',
-        isNewMsg: !0,
-        star: !1,
-        isForwarded: toFor.isForwarded || !toFor.isSentByMe,
-        forwardedFromWeb: !0,
-        forwardingScore: 21,
-        multicast: false,
-      };
-      Object.assign(tempMsg, toFor);
-      Object.assign(tempMsg, extend);
+  var m = { type: 'forwardMessages' };
 
-      return await Store.addAndSendMsgToChat(conversation, tempMsg);
-    }).then(async (t) => {
-      let result = await Promise.all(t);
-      console.log(result);
-    });
-  }
+  return new Promise(async (resolve, reject) => {
+    if (chat.id) {
+      var To = await WAPI.getchatId(chat.id);
+      await Promise.each(toForward, async (e) => {
+        if (typeof e.erro !== 'undefined' && e.erro === true) {
+          var obj = WAPI.scope(To, true, null, 'message not found');
+          Object.assign(obj, m);
+          reject(obj);
+          return;
+        }
+        let conversation = await window.Store.Chat.get(chatId);
+        let newId = await window.WAPI.getNewMessageId(chat.id);
+        let tempMsg = await Object.create(
+          chat.msgs.filter((msg) => msg.__x_isSentByMe)
+        )[0];
+        let toFor = await Object.assign(e);
+        let extend = {
+          id: newId,
+          from: Store.Me.wid,
+          t: parseInt(new Date().getTime() / 1000),
+          to: chat.__x_contact.__x_id,
+          ack: 0,
+          participant: void 0,
+          local: !0,
+          self: 'out',
+          isNewMsg: !0,
+          star: !1,
+          isForwarded: toFor.isForwarded || !toFor.isSentByMe,
+          forwardedFromWeb: !0,
+          forwardingScore: 21,
+          multicast: false,
+        };
+
+        Object.assign(tempMsg, toFor);
+        Object.assign(tempMsg, extend);
+
+        return await Store.addAndSendMsgToChat(conversation, tempMsg);
+      })
+        .then(async () => {
+          var obj = WAPI.scope(To, false, 200, null);
+          Object.assign(obj, m);
+          resolve(obj);
+        })
+        .catch(() => {
+          var obj = WAPI.scope(To, true, 404, null);
+          Object.assign(obj, m);
+          reject(obj);
+        });
+    } else {
+      reject(chat);
+    }
+  });
 }
