eb2b9afa0a867a48dda30caae8120b52dbe194fb
Jónalan de Lima <jonalan7@gmail.com>
2021-03-29 20:01:14 -0300

    fix: error undefined

--- src/api/helpers/layers-interface.ts
+++ src/api/helpers/layers-interface.ts
@@ -10,19 +10,21 @@ function infoType(data: string, type: string) {
 export function checkValuesSender(data: any) {
   if (Array.isArray(data)) {
     for (let i in data) {
-      let result =
-        data[i].type && data[i].value && infoType(data[i].value, data[i].type)
-          ? true
-          : !data[i].value && data[i].isUser === false
-          ? true
-          : false;
-      if (!result) {
-        return {
-          erro: true,
-          param: data[i].param,
-          function: data[i].function,
-          text: `Invalid parameter in ${data[i].param}! Pass a variable of type ${data[i].type}!`,
-        };
+      if (typeof i == 'number') {
+        let result =
+          data[i].type && data[i].value && infoType(data[i].value, data[i].type)
+            ? true
+            : !data[i].value && data[i].isUser === false
+            ? true
+            : false;
+        if (!result) {
+          return {
+            erro: true,
+            param: data[i].param,
+            function: data[i].function,
+            text: `Invalid parameter in ${data[i].param}! Pass a variable of type ${data[i].type}!`,
+          };
+        }
       }
     }
     return true;
