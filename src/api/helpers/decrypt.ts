60458e559d999eebe4ab4794d91fbaf9e3f01c06
Jónalan de Lima <jonalan7@gmail.com>
2021-04-11 03:20:42 -0300

    fix: update Agent

--- src/api/helpers/decrypt.ts
+++ src/api/helpers/decrypt.ts
@@ -82,8 +82,8 @@ export const mediaTypes = {
 const processUA = (userAgent: string) => {
   let ua =
     userAgent ||
-    'WhatsApp/2.16.352 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36';
-  if (!ua.includes('WhatsApp')) ua = 'WhatsApp/2.16.352 ' + ua;
+    'WhatsApp/2.2108.8 Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36';
+  if (!ua.includes('WhatsApp')) ua = 'WhatsApp/2.2108.8 ' + ua;
   return ua;
 };
 
