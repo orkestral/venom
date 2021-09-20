8e2eba60cd5b8ebc314962ba09ebd98168e39376
jonalan <jonalan7@gmail.com>
2021-09-20 14:02:04 -0300

    docs: i github

--- src/lib/wapi/functions/check-send-exist.js
+++ src/lib/wapi/functions/check-send-exist.js
@@ -144,7 +144,7 @@ export async function sendExist(chatId, returnChat = true, Send = true) {
   if (ck.status === 404 && !chatId.includes('@g.us')) {
     return WAPI.scope(chatId, true, ck.status, 'The number does not exist');
   }
-  
+
   let chat = await window.WAPI.getChat(ck.id._serialized);
 
   if (ck.numberExists && chat === undefined) {
