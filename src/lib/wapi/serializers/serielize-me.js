642aedcf3ccc0517df1e12d0f27cbe8eb1a09121
Jónalan de Lima <jonalan7@gmail.com>
2021-10-23 09:19:28 -0300

    fix: get profile

--- src/lib/wapi/serializers/serielize-me.js
+++ src/lib/wapi/serializers/serielize-me.js
@@ -3,8 +3,13 @@ export const _serializeMeObj = async (obj) => {
     return null;
   }
 
+  let appsImage = undefined;
   const status = await Store.MyStatus.getStatus(obj.wid._serialized);
-  const appsImage = await Store.Profile.profilePicFind(obj.wid._serialized);
+  await Store.Profile.profilePicFind(obj.wid._serialized)
+    .then((e) => {
+      appsImage = e;
+    })
+    .catch(() => {});
   const connection = await window.Store.State.default.state;
 
   return Object.assign(
