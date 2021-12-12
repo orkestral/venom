d4a4700557f1bbf632ba5f70a86477f146462fc6
Jónalan de Lima <jonalan7@gmail.com>
2021-12-12 14:54:14 -0300

    fix: sendContactVcard

--- src/lib/wapi/functions/send-contact-vcard.js
+++ src/lib/wapi/functions/send-contact-vcard.js
@@ -53,31 +53,64 @@ MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNMMNNNMMMMMMMMMMMMMMMMM
 MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 */
 export async function sendContactVcard(chatId, contact, name) {
+  if (typeof chatId != 'string' || chatId.length === 0) {
+    return WAPI.scope(chatId, true, 404, 'It is necessary to pass the number!');
+  }
+
+  if (typeof contact != 'string' || contact.length === 0) {
+    return WAPI.scope(
+      contact,
+      true,
+      404,
+      'It is necessary to pass the number!'
+    );
+  }
+
   const chat = await WAPI.sendExist(chatId);
   const cont = await WAPI.sendExist(contact);
-
-  if (chat.id && cont.id) {
+  if (
+    chat &&
+    chat.status != 404 &&
+    chat.id &&
+    cont &&
+    cont.status != 404 &&
+    cont.id
+  ) {
     const newMsgId = await window.WAPI.getNewMessageId(chat.id._serialized);
-    const inChat = await WAPI.getchatId(chat.id).catch(() => {});
+    let inChat = await WAPI.getchatId(chat.id).catch(() => {
+      return WAPI.scope(chat.id, true, 404, 'Error to number ' + chatId);
+    });
 
     if (inChat) {
-      chat.lastReceivedKey._serialized = inChat._serialized;
-      chat.lastReceivedKey.id = inChat.id;
+      chat.lastReceivedKey && chat.lastReceivedKey._serialized
+        ? (chat.lastReceivedKey._serialized = inChat._serialized)
+        : '';
+      chat.lastReceivedKey && chat.lastReceivedKey.id
+        ? (chat.lastReceivedKey.id = inChat.id)
+        : '';
+    }
+
+    if (!newMsgId) {
+      return WAPI.scope(chatId, true, 404, 'Error to newId');
     }
 
     const fromwWid = await Store.MaybeMeUser.getMaybeMeUser();
-    var body = await window.Store.Vcard.vcardFromContactModel(cont.__x_contact);
+    const body = await window.Store.Vcard.vcardFromContactModel(
+      cont.__x_contact
+    );
+
     name = !name ? cont.__x_formattedTitle : name;
-    var message = {
+
+    const message = {
       id: newMsgId,
       ack: 0,
       body: body.vcard,
       from: fromwWid,
-      self: 'in',
-      vcardFormattedName: name,
-      isNewMsg: true,
-      t: parseInt(new Date().getTime() / 1000),
       to: chat.id,
+      local: !0,
+      self: 'out',
+      t: parseInt(new Date().getTime() / 1000),
+      isNewMsg: !0,
       type: 'vcard'
     };
 
