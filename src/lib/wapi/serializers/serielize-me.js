5d72a61c4782009f19dd841ac5191a0e3a001721
Jónalan de Lima <jonalan7@gmail.com>
2021-12-19 06:35:30 -0300

    feat: SerializeMeObj

--- src/lib/wapi/serializers/serielize-me.js
+++ src/lib/wapi/serializers/serielize-me.js
@@ -4,15 +4,27 @@ export const _serializeMeObj = async (obj) => {
   }
 
   const connection = await window.Store.State.Socket.state;
+
   let PicThumb = undefined;
-  if (!obj.id.contact.profilePicThumb) {
+  if (
+    obj &&
+    obj.id &&
+    obj.id.contact &&
+    !obj.id.contact.profilePicThumb &&
+    obj.id.id
+  ) {
     PicThumb = await window.Store.ProfilePicThumb.get(obj.id.id._serialized);
   }
+
   const newObj = {};
+  if (!obj.id) {
+    return newObj;
+  }
+
   Object.assign(newObj, {
-    id: obj.id.id,
-    email: obj.id.email,
-    description: obj.id.description,
+    id: obj.id.id ? obj.id.id : null,
+    email: obj.id.email ? obj.id.email : null,
+    description: obj.id.description ? obj.id.description : null,
     statusConnection: connection,
     businessHours: {
       timezone:
@@ -24,11 +36,17 @@ export const _serializeMeObj = async (obj) => {
       obj.id.contact && obj.id.contact.pushname
         ? obj.id.contact.pushname
         : null,
-    website: obj.id.website,
-    formattedTitle: obj.id.formattedTitle,
-    categories: obj.id.categories,
-    displayName: obj.id.contact.displayName,
-    isBusiness: obj.id.contact.isBusiness,
+    website: obj.id.website ? obj.id.website : null,
+    formattedTitle: obj.id.formattedTitle ? obj.id.formattedTitle : null,
+    categories: obj.id.categories ? obj.id.categories : null,
+    displayName:
+      obj.id.contact && obj.id.contact.displayName
+        ? obj.id.contact.displayName
+        : null,
+    isBusiness:
+      obj.id.contact && obj.id.contact.isBusiness
+        ? obj.id.contact.isBusiness
+        : null,
     imgUrl:
       obj.id.contact &&
       obj.id.contact.profilePicThumb &&
@@ -56,17 +74,30 @@ export const _serializeMeObj = async (obj) => {
   });
   const conn = Store.Me ? Store.Me : undefined;
   if (conn && conn.wid && conn.wid._serialized) {
-    if (newObj.id._serialized === conn.wid._serialized) {
+    if (
+      newObj.id &&
+      newObj.id._serialized &&
+      conn.wid &&
+      conn.wid._serialized &&
+      newObj.id._serialized === conn.wid._serialized &&
+      conn.phone
+    ) {
       Object.assign(newObj, {
-        binVersion: conn.binVersion,
-        allLocales: conn.allLocales,
-        battery: conn.battery,
+        binVersion: conn.binVersion ? conn.binVersion : null,
+        allLocales: conn.allLocales ? conn.allLocales : null,
+        battery: conn.battery ? conn.battery : null,
         phone: {
-          device_manufacturer: conn.phone.device_manufacturer,
-          device_model: conn.phone.device_model,
-          os_build_number: conn.phone.os_build_number,
-          os_version: conn.phone.os_version,
-          wa_version: conn.phone.wa_version
+          device_manufacturer: conn.phone.device_manufacturer
+            ? conn.phone.device_manufacturer
+            : null,
+          device_model: conn.phone.device_model
+            ? conn.phone.device_model
+            : null,
+          os_build_number: conn.phone.os_build_number
+            ? conn.phone.os_build_number
+            : null,
+          os_version: conn.phone.os_version ? conn.phone.os_version : null,
+          wa_version: conn.phone.wa_version ? conn.phone.wa_version : null
         }
       });
     }
