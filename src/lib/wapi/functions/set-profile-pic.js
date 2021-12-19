ddc81844c5fb76be3ece25d715bd032ed07fa36e
Jónalan de Lima <jonalan7@gmail.com>
2021-12-19 07:01:47 -0300

    fix: setProfilePic

--- src/lib/wapi/functions/set-profile-pic.js
+++ src/lib/wapi/functions/set-profile-pic.js
@@ -1,9 +1,11 @@
 export async function setProfilePic(obj, id) {
   if (!id) {
-    id = Store.MaybeMeUser.getMaybeMeUser();
-  } else {
-    id = window.Store.WidFactory.createWid(id);
+    id = await Store.MaybeMeUser.getMaybeMeUser();
   }
   let base64 = 'data:image/jpeg;base64,';
-  return await Store.Profile.sendSetPicture(id, base64 + obj.b, base64 + obj.a);
+  return await Store.Profile.sendSetPicture(
+    id._serialized,
+    base64 + obj.b,
+    base64 + obj.a
+  );
 }
