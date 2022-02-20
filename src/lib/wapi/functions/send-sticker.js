557d1ae8b07597d16a77a1a3c27de24240c85685
Jónalan de Lima <jonalan7@gmail.com>
2022-02-20 06:43:12 -0300

    fix: sendSticker

--- src/lib/wapi/functions/send-sticker.js
+++ src/lib/wapi/functions/send-sticker.js
@@ -53,41 +53,54 @@ MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNMMNNNMMMMMMMMMMMMMMMMM
 MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 */
 export async function sendSticker(sticker, chatId, metadata, type) {
-  var chat = await WAPI.sendExist(chatId);
+  const chat = await WAPI.sendExist(chatId);
 
   if (!chat.erro) {
-    var stick = new window.Store.Sticker.modelClass();
+    const newMsgId = await window.WAPI.getNewMessageId(chat.id._serialized);
+    const fromwWid = await Store.MaybeMeUser.getMaybeMeUser();
+    const inChat = await WAPI.getchatId(chat.id).catch(() => {
+      return WAPI.scope(chat.id, true, 404, 'Error to number ' + chatId);
+    });
+    if (inChat) {
+      chat.lastReceivedKey && chat.lastReceivedKey._serialized
+        ? (chat.lastReceivedKey._serialized = inChat._serialized)
+        : '';
+      chat.lastReceivedKey && chat.lastReceivedKey.id
+        ? (chat.lastReceivedKey.id = inChat.id)
+        : '';
+    }
+    const message = {
+      id: newMsgId,
+      ack: 0,
+      encFilehash: sticker.uploadhash,
+      from: fromwWid,
+      to: chat.id,
+      local: !0,
+      self: 'out',
+      t: parseInt(new Date().getTime() / 1000),
+      isNewMsg: !0,
+      type: 'sticker',
+      deprecatedMms3Url: sticker.clientUrl,
+      filehash: sticker.filehash,
+      mediaKey: sticker.mediaKey,
+      mimetype: 'image/webp',
+      height: metadata && metadata.height ? metadata.height : 512,
+      width: metadata && metadata.width ? metadata.width : 512
+    };
 
-    stick.deprecatedMms3Url = sticker.clientUrl;
-    stick.filehash = sticker.filehash;
-    stick.id = sticker.filehash;
-    stick.encFilehash = sticker.uploadhash;
-    stick.mediaKey = sticker.mediaKey;
-    stick.initialized = false;
-    stick.mediaData.__x_mediaStage = 'INIT';
-    stick.mimetype = 'image/webp';
-    stick.height = metadata && metadata.height ? metadata.height : 512;
-    stick.width = metadata && metadata.width ? metadata.width : 512;
+    const result = (
+      await Promise.all(window.Store.addAndSendMsgToChat(chat, message))
+    )[1];
 
-    await stick.initialize();
-
-    var result =
-      (await stick.sendToChat(chat, {
-        stickerIsFirstParty: false,
-        stickerSendOrigin: 6
-      })) || '';
-    var m = { type: type },
-      obj,
-      To = await WAPI.getchatId(chat.id);
-    if (result === 'OK') {
-      obj = WAPI.scope(To, false, result, null);
-      Object.assign(obj, m);
-      return obj;
-    } else {
-      obj = WAPI.scope(To, true, result, null);
+    const m = { type: type };
+    if (result === 'success' || result === 'OK') {
+      const obj = WAPI.scope(newMsgId, false, result, null);
       Object.assign(obj, m);
       return obj;
     }
+    const obj = WAPI.scope(newMsgId, true, result, null);
+    Object.assign(obj, m);
+    return obj;
   } else {
     return chat;
   }
