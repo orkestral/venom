5b9cdceedeea5b1090acfeb59e668687c956b3cc
Orkestral <66584466+orkestral@users.noreply.github.com>
2022-06-21 00:25:40 -0300

    Update delete-messages-all.js

--- src/lib/wapi/functions/delete-messages-all.js
+++ src/lib/wapi/functions/delete-messages-all.js
@@ -52,6 +52,7 @@ MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMNMNMMMNMMNNMMMMMMMMMMMM
 MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNMMNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 */
+
 export async function deleteMessagesAll(chatId, messageArray, onlyLocal) {
   var userId = new Store.WidFactory.createWid(chatId);
 
@@ -62,28 +63,38 @@ export async function deleteMessagesAll(chatId, messageArray, onlyLocal) {
     messageArray = [messageArray];
   }
 
-  let messagesToDelete = messageArray.map((msgId) => typeof msgId == "string" ? window.Store.Msg.get(msgId) : msgId
+  let messagesToDelete = messageArray
+    .map((msgId) =>
+      typeof msgId == 'string' ? window.Store.Msg.get(msgId) : msgId
     )
     .filter((x) => x);
-  if (!messagesToDelete) return true; 
-  let jobs = onlyLocal ? [conversation.sendDeleteMsgs ? conversation.sendDeleteMsgs(messagesToDelete, conversation) : Store.WapDeleteMsg.sendDeleteMsgs(conversation,messagesToDelete)]
+  if (!messagesToDelete) return true;
+  let jobs = onlyLocal
+    ? [
+        conversation.sendDeleteMsgs
+          ? conversation.sendDeleteMsgs(messagesToDelete, conversation)
+          : Store.WapDeleteMsg.sendDeleteMsgs(conversation, messagesToDelete)
+      ]
     : [
-      conversation.sendRevokeMsgs ? conversation.sendRevokeMsgs(
-        messagesToDelete.filter((msg) => msg.isSentByMe),
-        conversation
-      ) :
-      Store.WapDeleteMsg.sendRevokeMsgs(
-          conversation,
-          messagesToDelete.filter((msg) => msg.isSentByMe),
-        true
-        ),
-      conversation.sendDeleteMsgs ? conversation.sendDeleteMsgs(
-        messagesToDelete.filter((msg) => !msg.isSentByMe),
-        conversation
-      ) : Store.WapDeleteMsg.sendDeleteMsgs(
-          conversation,
-          messagesToDelete.filter((msg) => !msg.isSentByMe),
-        )
+        conversation.sendRevokeMsgs
+          ? conversation.sendRevokeMsgs(
+              messagesToDelete.filter((msg) => msg.isSentByMe),
+              conversation
+            )
+          : Store.WapDeleteMsg.sendRevokeMsgs(
+              conversation,
+              messagesToDelete.filter((msg) => msg.isSentByMe),
+              true
+            ),
+        conversation.sendDeleteMsgs
+          ? conversation.sendDeleteMsgs(
+              messagesToDelete.filter((msg) => !msg.isSentByMe),
+              conversation
+            )
+          : Store.WapDeleteMsg.sendDeleteMsgs(
+              conversation,
+              messagesToDelete.filter((msg) => !msg.isSentByMe)
+            )
       ];
   return Promise.all(jobs).then((_) => true);
 }
