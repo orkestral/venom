c7ef7f10b1ff53818923893698e4681e6d34bedd
Jonatan <19454127+jrrcdev@users.noreply.github.com>
2022-07-09 10:00:29 -0300

    fix: e.isLid is not a function
    This commit fix the e.isLid is not a function issue #1792 #1970

--- src/lib/wapi/functions/check-number-status.js
+++ src/lib/wapi/functions/check-number-status.js
@@ -30,7 +30,7 @@ export async function checkNumberStatus(id, conn = false) {
 
     if (WAPI.isBeta()) {
       return await Store.checkNumberBeta
-        .queryExists(id)
+        .queryExists(new Store.WidFactory.createWid(id))
         .then((result) => {
           if (!!result && typeof result === 'object') {
             const data = {
