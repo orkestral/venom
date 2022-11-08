3175e1e231631c84639022b7378c4b129926816a
Edgard Lorraine Messias <edgardmessias@gmail.com>
2022-11-08 11:38:09 -0300

    chore: Re-added poweredBy config

--- src/controllers/browser.ts
+++ src/controllers/browser.ts
@@ -146,6 +146,7 @@ export async function injectApi(page: Page) {
     .evaluate(() => {
       WPP.chat.defaultSendMessageOptions.createChat = true;
       WPP.conn.setKeepAlive(true);
+      WPP.config.poweredBy = 'Venom-Bot';
     })
     .catch(() => false);
   await page.addScriptTag({
