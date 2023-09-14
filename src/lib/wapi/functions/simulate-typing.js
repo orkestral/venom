793965aff8c2a597b514ceccc208d196bd6b2d20
9cb14c1ec0 <93002271+9cb14c1ec0@users.noreply.github.com>
2023-09-14 10:55:47 -0400

    Remove stopTyping from simulate-typing.js

--- src/lib/wapi/functions/simulate-typing.js
+++ src/lib/wapi/functions/simulate-typing.js
@@ -11,21 +11,3 @@ export async function startTyping(chatId) {
   await Store.ChatState.sendChatStateComposing(chat);
   return { status: 200 };
 }
-
-/**
- * Stops typing
- * @param {string} chatId
- */
-export async function stopTyping(chatId) {
-  const chat = window.Store.WidFactory.createWid(chatId);
-  if (!chat) {
-    throw {
-      error: true,
-      code: 'chat_not_found',
-      message: 'Chat not found',
-      chatId: chatId
-    };
-  }
-  await Store.ChatState.sendChatStatePaused(chat);
-  return { status: 200 };
-}
