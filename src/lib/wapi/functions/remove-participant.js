2d95c487fa3421e966e07addaafebdd7b68322e1
GabrielBdeC-TR <gabriel.decarvalho@thomsonreuters.com>
2024-04-25 10:41:10 -0300

    fix: remove participant

--- src/lib/wapi/functions/remove-participant.js
+++ src/lib/wapi/functions/remove-participant.js
@@ -1,30 +1,66 @@
-export async function removeParticipant(groupId, contactsId, done) {
+const { GROUP_ERRORS } = require('../types/group-errors')
+
+export async function removeParticipant(groupId, contactsId) {
   const chat = Store.Chat.get(groupId)
 
   if (!Array.isArray(contactsId)) {
     contactsId = [contactsId]
   }
 
-  contactsId = await Promise.all(contactsId.map((c) => WAPI.sendExist(c)))
-  contactsId = contactsId
-    .filter((c) => !c.erro && c.isUser)
-    .map((c) => chat.groupMetadata.participants.get(c.id))
-    .filter((c) => typeof c !== 'undefined')
-    .map((c) => c.id)
-
-  if (!contactsId.length) {
-    typeof done === 'function' && done(false)
-    return false
-  }
-
-  await window.Store.WapQuery.removeParticipants(chat.id, contactsId)
-
-  const participants = contactsId.map((c) =>
-    chat.groupMetadata.participants.get(c)
+  contactsId = contactsId.filter(
+    (item, index) => contactsId.indexOf(item) === index
   )
 
-  await window.Store.Participants.removeParticipants(chat, participants)
+  const result = {}
+  contactsId.forEach((contactId) => {
+    result[contactId] = { success: false }
+  })
 
-  typeof done === 'function' && done(true)
-  return true
+  contactsId = await Promise.all(
+    contactsId.map(async (contactId) => {
+      const contactObj = await WAPI.sendExist(contactId, true, false)
+      if (
+        !contactObj ||
+        contactObj.erro ||
+        contactObj.status === 404 ||
+        !contactObj.isUser
+      ) {
+        result[contactId].error = GROUP_ERRORS.INVALID_CONTACT_ID
+        return
+      }
+      return contactObj
+    })
+  )
+  contactsId = contactsId.filter((el) => el)
+
+  contactsId = contactsId
+    .map((c) => {
+      const participant = chat.groupMetadata.participants.get(c.id)
+      if (!participant) {
+        result[c.id].error = GROUP_ERRORS.CONTACT_NOT_IN_GROUP
+        return
+      }
+      return participant
+    })
+    .filter((el) => el)
+  /* .map((c) => c.id)
+  await window.Store.WapQuery.removeParticipants(chat.id, contactsId)
+  const participants = contactsId.map((c) =>
+    chat.groupMetadata.participants.get(c)
+  ) */
+
+  if (Object.keys(result).every((key) => result[key].error)) {
+    return result
+  }
+
+  try {
+    await window.Store.Participants.removeParticipants(chat, contactsId)
+    contactsId.forEach((c) => {
+      result[c.id].success = true
+    })
+  } catch (err) {
+    console.log(err)
+  }
+
+  return result
 }
