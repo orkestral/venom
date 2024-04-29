5953b2f997f33d71714cfe36452e5ebca91404a6
Geoff Hayman <geoff.hayman@txtsmarter.com>
2024-04-29 13:20:33 -0700

    Fixed isLoggedIn() - #2724

--- src/lib/wapi/functions/is-logged-in.js
+++ src/lib/wapi/functions/is-logged-in.js
@@ -1,7 +1,6 @@
 export function isLoggedIn(done) {
   // Contact always exists when logged in
-  const isLogged =
-    window.Store.Contact && window.Store.Contact.checksum !== undefined;
+  const isLogged = window.Store.Contact && window.Store.Contact.length > 0;
 
   if (done !== undefined) done(isLogged);
   return isLogged;
