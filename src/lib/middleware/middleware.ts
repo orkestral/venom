975827cc450b1caf517a324243419bf880f0135b
Geoff Hayman <geoff.hayman@txtsmarter.com>
2024-05-02 11:52:51 -0700

    Removed WAPI.waitNewAcknowledgements from middleware as it does not seem to be fully implemented. Move from addScriptTag to page.evaluate highlighted this issue

--- src/lib/middleware/middleware.ts
+++ src/lib/middleware/middleware.ts
@@ -13,8 +13,11 @@ enum ExposedFn {
   });
 });
 
+// This does not appear to be implemented anywhere right now and is breaking code injection via evaluate instead of addScriptTag
+/*
 (window as any).WAPI.waitNewAcknowledgements(function (data: any) {
   if (window[ExposedFn.OnAck]) {
     window[ExposedFn.OnAck](data);
   }
 });
+*/
